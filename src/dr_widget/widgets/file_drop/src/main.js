c57185ce2f017069657d508394e907637e3b994f
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-11-03 18:51:13 -0500

    the widget shows up in the local preview

--- src/dr_widget/widgets/file_drop/src/main.js
+++ src/dr_widget/widgets/file_drop/src/main.js
@@ -1,8 +1,7 @@
 import './app.css';
 import App from './FileDropWidget.svelte';
+import { mount } from 'svelte';
 
-const app = new App({
-  target: document.getElementById('app'),
+mount(App, {
+  target: document.getElementById('app')
 });
-
-export default app;
