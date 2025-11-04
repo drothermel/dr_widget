0d2114812883035c3751bad47a72fb5bf654618b
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-11-03 19:28:24 -0500

    make the widget show in the notebook

--- notebooks/file_widget.py
+++ notebooks/file_widget.py
@@ -7,6 +7,33 @@ app = marimo.App(width="columns")
 @app.cell
 def _():
     import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    from dr_widget import FileDropWidget
+
+    widget = FileDropWidget()
+    mo.vstack(
+        [
+            mo.md("### File Drop Demo"),
+            mo.md("Drag files into the drop zone and inspect the synced metadata."),
+            widget,
+        ]
+    )
+    return (widget,)
+
+
+@app.cell
+def _(widget):
+    widget.file_count
+    return
+
+
+@app.cell
+def _(widget):
+    widget.files
     return
 
 
