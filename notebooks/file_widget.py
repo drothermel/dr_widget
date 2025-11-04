bd3ac32f45197780645f0862b499d9256e7feaa4
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-11-03 19:10:57 -0500

    add marimo and test notebook

---
+++ notebooks/file_widget.py
@@ -0,0 +1,19 @@
+import marimo
+
+__generated_with = "0.17.6"
+app = marimo.App(width="columns")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
